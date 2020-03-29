45e38217d77f52062dd0ccaa95100500a0ef660b
PhilipeCastro <62783587+PhilipeCastro@users.noreply.github.com>
2020-03-28 23:08:44 -0400

    bleehh

---
+++ script.js
@@ -0,0 +1,13 @@
+var car = {
+	make: 'honda',
+	type: 'civic',
+	color: 'blue',
+	isTurnedOn: false,
+	numberOfWheels: 4,
+	seats: [
+	'seat1', 
+	'seat2', 
+	'seat3'
+		]
+	};
+console.log('whats up');
